0e92153561cfee955a73da229842b5eed2b6f7a3
Rafael Nascimento <85128772+Faterzin@users.noreply.github.com>
2026-05-05 09:32:08 -0300

    Estrutura base do schema

---
+++ schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE pacientes (
+    
+);
+
+CREATE TABLE medicos (
+    
+);
+
+CREATE TABLE especialidades (
+    
+);
+
+CREATE TABLE agendamentos (
+    
+);
+
+CREATE TABLE consultas (
+    
+);
+
+CREATE TABLE prescricoes (
+    
+);
+
+CREATE TABLE medicamentos (
+    
+);
+
+CREATE TABLE convenios (
+    
+);
