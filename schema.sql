4bc7335d8dd3fd0e2d02c59688f9e7ba07d691d8
bellatrixhelpme-ux <bellatrixhelpme@gmail.com>
2026-05-05 10:58:17 -0300

    adicionando consultas e especialidades no schema do banco de dados

--- schema.sql
+++ schema.sql
@@ -7,6 +7,10 @@ CREATE TABLE medicos (
 );
 
 CREATE TABLE especialidades (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    nome VARCHAR(100) NOT NULL UNIQUE,
+    descricao TEXT,
+    criada_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     
 );
 
@@ -15,11 +19,23 @@ CREATE TABLE agendamentos (
 );
 
 CREATE TABLE consultas (
-    
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id_agendamento UUID NOT NULL,
+    data_inicio DATETIME NOT NULL,
+    duracao_minutos INT NOT NULL default 30,
+    queixa_principal TEXT NOT NULL,
+    observacoes_medico TEXT,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
 );
 
 CREATE TABLE prescricoes (
-    
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id_consulta UUID NOT NULL,
+    id_medicamento UUID NOT NULL,
+    dosagem VARCHAR(50) NOT NULL,
+    frequencia VARCHAR(100) NOT NULL,
+    duracao_tratamento INT NOT NULL
+    criada_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
 CREATE TABLE medicamentos (
